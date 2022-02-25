65d27197a194bdb2fb35e2397435d0ed5420a4e6
mert18 <mertuygur02@gmail.com>
2022-02-25 21:23:14 +0300

    most basic form

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="h-screen w-screen flex justify-center items-center border-black border-4">
+      See branches for examples.
+    </div>
+  )
+}
+
+export default App;
