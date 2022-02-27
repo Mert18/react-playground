747bd8b0e531bc4793157c1fdce0d826f008b3a9
mert18 <mertuygur02@gmail.com>
2022-02-27 10:45:34 +0300

    tailwind jit configured

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,24 @@
-import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { getPosts } from "./store/posts/postsSlice";
 
 const App = () => {
   const dispatch = useDispatch();
 
-  useEffect(() => {
+  const handleClick = () => {
     dispatch(getPosts());
-  }, [dispatch]);
+  };
 
   return (
-    <div>
-      You are in master branch. Folder structure and redux implementation lies
-      here.
+    <div className="text-[1rem] flex flex-col justify-center items-center h-[100vh]">
+      <p>
+        You are in master branch. Folder structure and redux implementation lies
+        here.
+      </p>
+      <p>Open Redux DevTools and click the button to test redux async logic.</p>
       <div>
-        <button>Click</button>
+        <button className="border-8" onClick={handleClick}>
+          Click
+        </button>
       </div>
     </div>
   );
